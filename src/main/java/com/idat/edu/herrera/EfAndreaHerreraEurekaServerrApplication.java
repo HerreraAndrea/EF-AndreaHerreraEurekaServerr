fcee99739c8f7858a8b9589069acb4c3caf072bb
HerreraAndrea <andreaherreravp@gmail.com>
2022-08-16 23:31:26 -0500

    Eureka Server

---
+++ src/main/java/com/idat/edu/herrera/EfAndreaHerreraEurekaServerrApplication.java
@@ -0,0 +1,15 @@
+package com.idat.edu.herrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EfAndreaHerreraEurekaServerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAndreaHerreraEurekaServerrApplication.class, args);
+	}
+
+}
